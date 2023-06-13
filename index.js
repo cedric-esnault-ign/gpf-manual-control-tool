96a8048848936f6e6c5cc3b01f3e5936341c0ca2
amaury.zarzelli <amaury.zarzelli@ign.fr>
2023-06-13 15:55:44 +0200

    GPP url until GPF is OK

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const center = [48.84497573035927, 2.4247001358237523];
 
 function convertToGpfURL(gppURL) {
-  return "https://wmts.geopf.fr/rok4/wmts?"
+  // return "https://wmts.geopf.fr/rok4/wmts?"
+  return gppURL
 }
 
 const mapGpp = L.map('mapGpp', {
